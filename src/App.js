044fde98c16d9f9ede6d56526579572afc981042
Fujita kz <fujitakz@AG000439noMacBook-Pro.local>
2019-10-18 14:02:57 +0900

    feat: add module: VRScene

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'aframe';
+import {Entity, Scene} from 'aframe-react';
 
 function App() {
   return (
     <div className="App">
+      <VRScene />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,6 +24,17 @@ function App() {
       </header>
     </div>
   );
+}
+
+const VRScene = () => {
+  return (
+    <Scene>
+      <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
+      <Entity light={{type: 'point'}}/>
+      <Entity gltf-model={{src: 'virtualcity.gltf'}}/>
+      <Entity text={{value: 'Hello, WebVR!'}}/>
+    </Scene>  
+  )
 }
 
 export default App;
