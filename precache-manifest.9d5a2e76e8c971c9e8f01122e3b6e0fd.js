41caba3480030c0e5c7a4ebf698a62e790c257ef
kfjt <kfjt@users.noreply.github.com>
2019-10-18 03:13:36 +0000

    Automated deployment: Fri Oct 18 03:13:36 UTC 2019 96f9335afe732186483df5813de3d1b6836b3dda

---
+++ precache-manifest.9d5a2e76e8c971c9e8f01122e3b6e0fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d36b3da8f04fb2de0f579913cc659b",
+    "url": "/webvr/index.html"
+  },
+  {
+    "revision": "3670f79956499d98efbc",
+    "url": "/webvr/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ac5915d8bc64c6aaef4a",
+    "url": "/webvr/static/js/2.2aac7587.chunk.js"
+  },
+  {
+    "revision": "3670f79956499d98efbc",
+    "url": "/webvr/static/js/main.d0d830c1.chunk.js"
+  },
+  {
+    "revision": "e3359c297130bdb5c1df",
+    "url": "/webvr/static/js/runtime-main.364ec6ae.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/webvr/static/media/logo.25bf045c.svg"
+  }
+]);
