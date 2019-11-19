57b3440c1af2489913b1a90da9111108964a4377
kfjt <k.fujita@jcom.home.ne.jp>
2019-11-19 10:14:54 +0900

    feat: sky use src

--- src/VRScene.js
+++ src/VRScene.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from 'react'
+import logo from './logo.svg'
 
 const VRScene = () => {
   return (
@@ -7,7 +8,10 @@ const VRScene = () => {
       <a-entity geometry="primitive: sphere" position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-entity>
       <a-entity geometry="primitive: cylinder" position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-entity>
       <a-entity geometry="primitive: plane" position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-entity>
-      <a-sky color="#ECECEC"></a-sky>
+      <a-assets>
+        <img id="sky" alt='sky' src={logo} />
+      </a-assets>
+      <a-sky src="#sky"></a-sky>
     </a-scene>
   )
 }
