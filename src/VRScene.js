f31979fd3be767a349ab13a9b2155e0dd8560cb8
kfjt <k.fujita@jcom.home.ne.jp>
2019-11-20 06:42:13 +0900

    feat: add img 360

--- src/VRScene.js
+++ src/VRScene.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from 'react'
+import back from './landscape-3531355_960_720.jpg'
 
 const Video = props => {
   const { videoEl } = props
@@ -8,7 +9,7 @@ const Video = props => {
     const setVideoStream = async () => {
       video.srcObject = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
     }
-    video.addEventListener('loadeddata', video.play)
+    // video.addEventListener('loadeddata', video.play)
     setVideoStream()
   }, [videoEl])
 
@@ -25,28 +26,34 @@ const VRScene = () => {
     video.addEventListener('loadedmetadata', () => {
       canvas.width = video.videoWidth
       canvas.height = video.videoHeight
-      video.style.display = 'none'
+      // video.style.display = 'none'
     })
     const updateCanvas = () => {
+      // console.count('updateCanvas')
       const ctx = canvas.getContext('2d')
       ctx.drawImage(video, 0, 0)
       requestAnimationFrame(updateCanvas)
     }
+    video.addEventListener('loadeddata', video.play)
     video.addEventListener('loadeddata', updateCanvas)
+
+    return () => cancelAnimationFrame(updateCanvas)
   })
 
   return (
     <div className='vrscene'>
       <Video videoEl={videoEl} />
+      {/* <canvas ref={canvasEl} /> */}
       <a-scene>
         <a-entity geometry="primitive: box" position="-1 0.5 -3" rotation="0 45 0" material="color:#4CC3D9"></a-entity>
         <a-entity geometry="primitive: sphere" position="0 1.25 -5" radius="1.25" material="color:#EF2D5E"></a-entity>
         <a-entity geometry="primitive: cylinder" position="1 0.75 -3" radius="0.5" height="1.5" material="color:#FFC65D"></a-entity>
         <a-entity geometry="primitive: plane" position="0 0 -4" rotation="-90 0 0" width="4" height="4" material="color:#7BC8A4"></a-entity>
         <a-assets>
-          <canvas id='sky' ref={canvasEl} />
+          <canvas id='_sky' ref={canvasEl} />
+          <img id='sky' alt='sky' src={back} />
         </a-assets>
-        <a-sky src="#sky"></a-sky>
+        <a-sky src="#sky" />
       </a-scene>
     </div>
   )
