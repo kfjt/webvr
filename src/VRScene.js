87903451809a1069f3662e6a1a45ee78620dd6ec
kfjt <k.fujita@jcom.home.ne.jp>
2019-11-22 07:13:37 +0900

    feat: WebCamTexture

--- src/VRScene.js
+++ src/VRScene.js
@@ -1,59 +1,29 @@
 import React, { useEffect, useRef, useState } from 'react'
-// import back from './landscape-3531355_960_720.jpg'
-
-const Video = ({ videoEl }) => {
-  useEffect(() => {
-    const video = videoEl.current
-    const setVideoStream = async () => {
-      video.srcObject = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment', frameRate: { ideal: 2, max: 10 } } })
-    }
-    setVideoStream()
-  }, [videoEl])
-
-  return <video ref={videoEl} />
-}
-
-const Sky = ({ dataUrl }) => dataUrl ? <img id='sky' alt='sky' src={dataUrl} /> : <span id='sky' />
 
 const VRScene = () => {
   const videoEl = useRef()
-  const canvasEl = useRef()
-  const [back, setBack] = useState()
+  const [stream, setStream] = useState()
+  const videoSource = async () => {
+    setStream(await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment', frameRate: { ideal: 2, max: 10 } } }))
+  }
 
   useEffect(() => {
+    videoSource()
     const video = videoEl.current
-    const canvas = canvasEl.current
-    video.addEventListener('loadedmetadata', () => {
-      canvas.width = video.videoWidth
-      canvas.height = video.videoHeight
-      video.style.display = 'none'
-    })
-    const updateCanvas = () => {
-      const ctx = canvas.getContext('2d')
-      ctx.drawImage(video, 0, 0)
-      setBack(canvas.toDataURL())
-      requestAnimationFrame(updateCanvas)
-    }
     video.addEventListener('loadeddata', video.play)
-    video.addEventListener('loadeddata', updateCanvas)
-
-    return () => cancelAnimationFrame(updateCanvas)
-  }, [])
+    video.srcObject = stream
+  })
 
   return (
     <div className='vrscene'>
-      <Video videoEl={videoEl} />
-      <canvas ref={canvasEl} />
       <a-scene>
         <a-assets>
-          <Sky dataUrl={back} />
+          <video id='webcam' crossOrigin='anonymous' ref={videoEl} />
         </a-assets>
-        <a-image src="#sky" />
-        <a-entity geometry="primitive: box" position="-1 0.5 -3" rotation="0 45 0" material="color:#4CC3D9"></a-entity>
-        <a-entity geometry="primitive: sphere" position="0 1.25 -5" radius="1.25" material="color:#EF2D5E"></a-entity>
-        <a-entity geometry="primitive: cylinder" position="1 0.75 -3" radius="0.5" height="1.5" material="color:#FFC65D"></a-entity>
-        <a-entity geometry="primitive: plane" position="0 0 -4" rotation="-90 0 0" width="4" height="4" material="color:#7BC8A4"></a-entity>
-        {/* <a-sky src="#sky" /> */}
+        <a-entity geometry="primitive: box" position="-1 0.5 -3" rotation="0 45 0" material="src: #webcam"></a-entity>
+        <a-entity geometry="primitive: sphere" position="0 1.25 -5" radius="1.25" material="src: #webcam"></a-entity>
+        <a-entity geometry="primitive: cylinder" position="1 0.75 -3" radius="0.5" height="1.5" material="src: #webcam"></a-entity>
+        <a-entity geometry="primitive: plane" position="0 0 -4" rotation="-90 0 0" width="4" height="4" material="src: #webcam"></a-entity>
       </a-scene>
     </div>
   )
