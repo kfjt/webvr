de26a002e91615668efb2fcff7d9b8f638cd0b50
kfjt <k.fujita@jcom.home.ne.jp>
2019-11-21 12:57:17 +0900

    feat: image instead of sky

--- src/VRScene.js
+++ src/VRScene.js
@@ -1,24 +1,24 @@
-import React, { useEffect, useRef } from 'react'
-import back from './landscape-3531355_960_720.jpg'
+import React, { useEffect, useRef, useState } from 'react'
+// import back from './landscape-3531355_960_720.jpg'
 
-const Video = props => {
-  const { videoEl } = props
-
+const Video = ({ videoEl }) => {
   useEffect(() => {
     const video = videoEl.current
     const setVideoStream = async () => {
-      video.srcObject = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
+      video.srcObject = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment', frameRate: { ideal: 2, max: 10 } } })
     }
-    // video.addEventListener('loadeddata', video.play)
     setVideoStream()
   }, [videoEl])
 
   return <video ref={videoEl} />
 }
 
+const Sky = ({ dataUrl }) => dataUrl ? <img id='sky' alt='sky' src={dataUrl} /> : <span id='sky' />
+
 const VRScene = () => {
   const videoEl = useRef()
   const canvasEl = useRef()
+  const [back, setBack] = useState()
 
   useEffect(() => {
     const video = videoEl.current
@@ -26,34 +26,34 @@ const VRScene = () => {
     video.addEventListener('loadedmetadata', () => {
       canvas.width = video.videoWidth
       canvas.height = video.videoHeight
-      // video.style.display = 'none'
+      video.style.display = 'none'
     })
     const updateCanvas = () => {
-      // console.count('updateCanvas')
       const ctx = canvas.getContext('2d')
       ctx.drawImage(video, 0, 0)
+      setBack(canvas.toDataURL())
       requestAnimationFrame(updateCanvas)
     }
     video.addEventListener('loadeddata', video.play)
     video.addEventListener('loadeddata', updateCanvas)
 
     return () => cancelAnimationFrame(updateCanvas)
-  })
+  }, [])
 
   return (
     <div className='vrscene'>
       <Video videoEl={videoEl} />
-      {/* <canvas ref={canvasEl} /> */}
+      <canvas ref={canvasEl} />
       <a-scene>
+        <a-assets>
+          <Sky dataUrl={back} />
+        </a-assets>
+        <a-image src="#sky" />
         <a-entity geometry="primitive: box" position="-1 0.5 -3" rotation="0 45 0" material="color:#4CC3D9"></a-entity>
         <a-entity geometry="primitive: sphere" position="0 1.25 -5" radius="1.25" material="color:#EF2D5E"></a-entity>
         <a-entity geometry="primitive: cylinder" position="1 0.75 -3" radius="0.5" height="1.5" material="color:#FFC65D"></a-entity>
         <a-entity geometry="primitive: plane" position="0 0 -4" rotation="-90 0 0" width="4" height="4" material="color:#7BC8A4"></a-entity>
-        <a-assets>
-          <canvas id='_sky' ref={canvasEl} />
-          <img id='sky' alt='sky' src={back} />
-        </a-assets>
-        <a-sky src="#sky" />
+        {/* <a-sky src="#sky" /> */}
       </a-scene>
     </div>
   )
