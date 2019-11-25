284ddfba263031945b87e0e75f9128e8c79967c6
kfjt <k.fujita@jcom.home.ne.jp>
2019-11-25 15:49:03 +0900

    feat: RealWorld

--- src/VRScene.js
+++ src/VRScene.js
@@ -1,18 +1,43 @@
 import React, { useEffect, useRef, useState } from 'react'
 import 'aframe'
 
+const RealWorld = ({ videoEl }) => {
+  const width = useRef(0)
+  const height = useRef(0)
+  const depth = useRef(0)
+
+  useEffect(() => {
+    const video = videoEl.current
+    video.addEventListener('loadeddata', () => {
+      width.current = video.videoWidth
+      height.current = video.videoHeight
+      depth.current = video.videoHeight / 2
+    })
+  })
+
+  if (width.current === 0 || height.current === 0)
+    return <div>camera loading ...</div>
+
+  return (
+    <a-entity
+      geometry={`primitive: plane; width: ${width.current}; height: ${height.current}`}
+      material="src: #webcam"
+      position={`0 0 -${depth.current}`} />
+  )
+}
+
 const VRScene = () => {
   const videoEl = useRef()
   const [stream, setStream] = useState()
-  const videoSource = async () => {
-    setStream(await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment', frameRate: { ideal: 2, max: 10 } } }))
-  }
 
   useEffect(() => {
-    videoSource()
     const video = videoEl.current
     video.addEventListener('loadeddata', video.play)
     video.srcObject = stream
+    const setVideoSource = async () => {
+      setStream(await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment', frameRate: { ideal: 2, max: 10 } } }))
+    }
+    setVideoSource()
   })
 
   return (
@@ -21,7 +46,9 @@ const VRScene = () => {
         <a-asset>
           <video id='webcam' ref={videoEl} />
         </a-asset>
-        <a-entity geometry="primitive: plane; height: 100; width: 100" position="0 0 -30" material="src: #webcam"></a-entity>
+        <a-entity camera look-controls>
+          <RealWorld videoEl={videoEl} />
+        </a-entity>
         <a-entity geometry="primitive: box" position="-1 0.5 -3" rotation="0 45 0" material="color: yellow"></a-entity>
         <a-entity geometry="primitive: sphere" position="0 1.25 -5" radius="1.25" material="color: pink"></a-entity>
         <a-entity geometry="primitive: cylinder" position="1 0.75 -3" radius="0.5" height="1.5" material="color: red"></a-entity>
