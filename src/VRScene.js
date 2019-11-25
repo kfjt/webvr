c611f8ec3adcb6af08763cccab346f1e5bc18b2f
kfjt <k.fujita@jcom.home.ne.jp>
2019-11-25 13:03:21 +0900

    feat: add size to entity

--- src/VRScene.js
+++ src/VRScene.js
@@ -17,15 +17,16 @@ const VRScene = () => {
 
   return (
     <div className='vrscene'>
-      <a-scene>
+      <a-scene stats>
         <a-asset>
-          <video id='webcam' crossOrigin='anonymous' ref={videoEl} />
+          <video id='webcam' ref={videoEl} />
         </a-asset>
+        <a-entity geometry="primitive: plane; height: 100; width: 100" position="0 0 -30" material="src: #webcam"></a-entity>
         <a-entity geometry="primitive: box" position="-1 0.5 -3" rotation="0 45 0" material="color: yellow"></a-entity>
         <a-entity geometry="primitive: sphere" position="0 1.25 -5" radius="1.25" material="color: pink"></a-entity>
         <a-entity geometry="primitive: cylinder" position="1 0.75 -3" radius="0.5" height="1.5" material="color: red"></a-entity>
-        <a-entity geometry="primitive: plane" position="0 0 -4" rotation="-90 0 0" width="4" height="4" material="color: purple"></a-entity>
-        <a-sky src="#webcam"></a-sky>
+        <a-entity geometry="primitive: plane; height: 4; width: 4" position="0 0 -4" rotation="-90 0 0" material="color: purple"></a-entity>
+        <a-sky material="color: black" />
       </a-scene>
     </div>
   )
