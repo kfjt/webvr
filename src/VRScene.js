618cdf7cf4b0baba7718cdefd8d058d4b91c0b92
kfjt <k.fujita@jcom.home.ne.jp>
2019-11-26 12:05:14 +0900

    feat: loading

--- src/VRScene.js
+++ src/VRScene.js
@@ -2,28 +2,30 @@ import React, { useEffect, useRef, useState } from 'react'
 import 'aframe'
 
 const RealWorld = ({ videoEl }) => {
-  const width = useRef(0)
-  const height = useRef(0)
-  const depth = useRef(0)
+  const aplaneRef = useRef()
+  const [pause, setPause] = useState(true)
+  const showVideo = () => { setPause(false) }
 
   useEffect(() => {
     const video = videoEl.current
+    const aplane = aplaneRef.current
     video.addEventListener('loadeddata', () => {
-      width.current = video.videoWidth
-      height.current = video.videoHeight
-      depth.current = video.videoHeight / 2
+      if (!pause) return
+      aplane.setAttribute('text', { value: '' })
+      aplane.setAttribute('geometry', { primitive: 'plane' })
+      aplane.setAttribute('geometry', { width: video.videoWidth, height: video.videoHeight })
+      aplane.setAttribute('position', { x: 0, y: 0, z: -1 * (video.videoHeight / 2) })
+      aplane.setAttribute('material', 'src', '#webcam')
+      showVideo()
     })
+    if (pause) {
+      aplane.setAttribute('text', { value: 'camera loading ...' })
+      aplane.setAttribute('geometry', { width: 800, height: 600 })
+      aplane.setAttribute('position', { x: 0, y: 0, z: -400 })
+    }
   })
 
-  if (width.current === 0 || height.current === 0)
-    return <div>camera loading ...</div>
-
-  return (
-    <a-entity
-      geometry={`primitive: plane; width: ${width.current}; height: ${height.current}`}
-      material="src: #webcam"
-      position={`0 0 -${depth.current}`} />
-  )
+  return <a-entity ref={aplaneRef} />
 }
 
 const VRScene = () => {
@@ -46,13 +48,13 @@ const VRScene = () => {
         <a-asset>
           <video id='webcam' ref={videoEl} />
         </a-asset>
-        <a-entity camera look-controls>
+        <a-entity camera look-controls wasd-controls position="0 1.6 4">
           <RealWorld videoEl={videoEl} />
         </a-entity>
         <a-entity geometry="primitive: box" position="-1 0.5 -3" rotation="0 45 0" material="color: yellow"></a-entity>
         <a-entity geometry="primitive: sphere" position="0 1.25 -5" radius="1.25" material="color: pink"></a-entity>
         <a-entity geometry="primitive: cylinder" position="1 0.75 -3" radius="0.5" height="1.5" material="color: red"></a-entity>
-        <a-entity geometry="primitive: plane; height: 4; width: 4" position="0 0 -4" rotation="-90 0 0" material="color: purple"></a-entity>
+        <a-entity geometry="primitive: plane; height: 2; width: 2" position="0 0 -4" rotation="-90 0 0" material="color: purple"></a-entity>
         <a-sky material="color: black" />
       </a-scene>
     </div>
