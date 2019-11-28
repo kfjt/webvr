e1202c7a1ae591aa91b3b094db2280330abffa25
kfjt <k.fujita@jcom.home.ne.jp>
2019-11-28 11:06:55 +0900

    feat:canvas

--- src/VRScene.js
+++ src/VRScene.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import styled from 'styled-components'
+
+const Canvas = styled.canvas`
+position: absolute
+`
+
+const use2dText = ({ refCanvas, text, coord }) => {
+  useEffect(() => {
+    const canvas = refCanvas.current
+    const context = canvas.getContext("2d")
+    context.fillText(text, coord.x, coord.y, 40)
+  })
+}
 
 const VRScene = () => {
-  const gltfUrl = 'https://modelviewer.dev/examples/assets/Astronaut.'
+  const refC1 = useRef()
+  const refC2 = useRef()
+  use2dText({ refCanvas: refC1, text: 'Hello', coord: { x: 20, y: 20 } })
+  use2dText({ refCanvas: refC2, text: 'World', coord: { x: 20, y: 40 } })
+
   return (
     <div className='vrscene'>
-      <model-viewer 
-        src={`${gltfUrl}glb`} 
-        ar 
-        camera-controls 
-        alt="A 3D model of an astronaut" 
-        background-color="#222" 
-        ios-src={`${gltfUrl}usdz`} 
-        magic-leap 
-        unstable-webxr></model-viewer>
+      <Canvas ref={refC1} className='hello' />
+      <Canvas ref={refC2} className='world' />
     </div>
   )
 }
