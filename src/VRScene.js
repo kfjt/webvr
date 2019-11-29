4ebc1c7161eaaee82aae8ab826b0da8db8e5f0b4
kfjt <k.fujita@jcom.home.ne.jp>
2019-11-29 15:32:04 +0900

    chore: aframe only

--- src/VRScene.js
+++ src/VRScene.js
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react'
 import styled from 'styled-components'
 
 import { Video, useVideo } from './Video'
-import { use2dText, useGl, useThree } from './myEffect'
+// import { use2dText, useGl, useThree } from './myEffect'
 import { AFrameScene } from './Aframe'
 
 const Div = styled.div`
@@ -10,31 +10,31 @@ const Div = styled.div`
   height: ${props => props.height}px
 `
 
-const Canvas = styled.canvas`
-  position: absolute
-  width: ${props => props.width}px
-  height: ${props => props.height}px
-`
+// const Canvas = styled.canvas`
+//   position: absolute
+//   width: ${props => props.width}px
+//   height: ${props => props.height}px
+// `
 
 const VRScene = () => {
   const refV = useRef()
-  const refC1 = useRef()
-  const refC2 = useRef()
-  const refC3 = useRef()
+  // const refC1 = useRef()
+  // const refC2 = useRef()
+  // const refC3 = useRef()
   const [width, setWidth] = useState()
   const [height, setHeight] = useState()
 
   useVideo({ refVideo: refV, setWidth, setHeight })
-  useGl({ refCanvas: refC1 })
-  use2dText({ refCanvas: refC2, text: 'Hello World', coord: { x: 20, y: 40 } })
-  useThree({ refCanvas: refC3 })
+  // useGl({ refCanvas: refC1 })
+  // use2dText({ refCanvas: refC2, text: 'Hello World', coord: { x: 20, y: 40 } })
+  // useThree({ refCanvas: refC3 })
 
   return (
     <Div className='vrscene' width={width} height={height}>
       <Video ref={refV} />
-      <Canvas ref={refC1} width={width} height={height} />
-      <Canvas ref={refC2} width={width} height={height} />
-      <Canvas ref={refC3} width={width} height={height} />
+      {/* <Canvas ref={refC1} width={width} height={height} /> */}
+      {/* <Canvas ref={refC2} width={width} height={height} /> */}
+      {/* <Canvas ref={refC3} width={width} height={height} /> */}
       <AFrameScene width={width} height={height}></AFrameScene>
     </Div>
   )
